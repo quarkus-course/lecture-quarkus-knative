dd7e5a69a5aee1547a987a7724c5fc0eff16deb8
dmi3coder <dmi3coder@gmail.com>
2020-04-12 10:58:52 +0300

    Initial commit for quarkus-knative

---
+++ src/main/java/tech/donau/WeatherResource.java
@@ -0,0 +1,17 @@
+package tech.donau;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/weather")
+public class WeatherResource {
+
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}
